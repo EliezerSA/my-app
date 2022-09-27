0e8375c13b6cf3bac63b2e94c00041013ad4bcb3
Marta98 <eliezersantiago-100@outlook.com>
2022-09-27 12:53:05 -0500

    Nombre del Proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/pedro/myapp/entity/TestEntity.java
@@ -0,0 +1,23 @@
+package com.pedro.myapp.entity;
+
+public class TestEntity {
+	private String nombre;
+
+	/*
+	 * Prpiedad de prueba para la muestra de compliacion del archivo testentity*
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
